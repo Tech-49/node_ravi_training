7e4396c9fc8b70da60106177407e3be27f2a3cc9
raviraviya <raviya.ravi1212@gmail.com>
2023-08-31 12:03:59 +0530

    day2

--- PDFs/Ravi/Day2/callback.js
+++ PDFs/Ravi/Day2/callback.js
@@ -3,11 +3,11 @@
 // Promise
 // Async/Await
 
-/*onst skills = ["Node.js", "React", "Python"];
+/*const skills = ["Node.js", "React", "Python"];
 
 function getUser(callback) {
     setTimeout(function () {
-        callback({ id: 1, name: "Hardik" });
+    callback({ id: 1, name: "Hardik" });
     }, 2000);
 }
 
@@ -22,9 +22,65 @@ console.log("After");*/
 
 
 
-console.log("First program Run")
 
-setTimeout(function () {
-    console.log("Third program Run")
-}, 2000)
-console.log("Second program Run")
+
+
+
+function getSkills(callback) {
+    setTimeout(function () {
+        callback(["Node.js", "React", "Python", "php", "javaScript"]);
+    }, 4000)
+}
+
+
+function getUser(callback) {
+    setTimeout(function () {
+        callback({ id: 1, name: "Ravi", id: 2, name: "Hardik", id: 3, name: "John" });
+    }, 2000)
+}
+
+getUser(function (result) {
+    console.log(result)
+})
+
+getSkills(function (userSkills) {
+    console.log(userSkills)
+});
+
+
+
+
+let a = 10;
+function number() {
+    setTimeout(function () {
+        if (a % 2 == 0) {
+            console.log("The number is even");
+        }
+        else {
+            console.log("The number is odd")
+        }
+    }, 4000)
+}
+number()
+
+
+
+
+
+
+
+function sayHello() {
+    console.log("hello...")
+}
+function sayHi() {
+    console.log("hi...")
+}
+
+let x = 10;
+let y = 20;
+function add(num1, num2, callback) {
+    console.log(num1 + num2);
+    callback()
+}
+add(x, y, sayHello)
+add(10, 30, sayHi)
