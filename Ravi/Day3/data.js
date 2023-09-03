ba96e62b7a2cad5a72a8c96b1c41675b3f656d11
raviraviya <raviya.ravi1212@gmail.com>
2023-09-03 10:54:32 +0530

    path

--- Ravi/Day3/data.js
+++ Ravi/Day3/data.js
@@ -1,5 +1,6 @@
 function user() {
-    console.log(["ravi,hardik,john"])
+    l
+    console.log(["ravi", "hardik", "john"])
 }
 
 module.exports.user = user;
--- Ravi/Day3/module.js
+++ Ravi/Day3/module.js
@@ -2,3 +2,8 @@ const path = require("path")
 
 let result = path.parse("D:\JavaScript ALL\ifelse");
 console.log(result)
+console.log(result.ext)
+console.log(result.dir)
+
+const os = require("os")
+console.log(os.freemem)
